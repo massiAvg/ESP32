ba57f4670c1cd3e5bded8c8c705656a92ce76a41
massi.av <massimilianoavagliano@gmail.com>
2022-07-05 13:06:35 +0200

    added JsonSerialize

--- src/MQTTConnect/MQTTConnect.ino
+++ src/MQTTConnect/MQTTConnect.ino
@@ -1,17 +1,17 @@
 /*
-  SimpleMQTTClient.ino
-  The purpose of this exemple is to illustrate a simple handling of MQTT and Wifi connection.
-  Once it connects successfully to a Wifi network and a MQTT broker, it subscribe to a topic and send a message to it.
-  It will also send a message delayed 5 seconds later.
+*         Created on  : 04/07/22
+*         Last Update : 05/07/22
 */
 
 #include "EspMQTTClient.h"
+#include <ArduinoJson.h>
 
-String topic = "myTopic/test";
+String topic = "myTopic/massiAvg";
+StaticJsonDocument<200> msg;
 
 EspMQTTClient client(
-  "WiFiSSID",
-  "WiFiPassword",
+  "wifiSsid",
+  "wifiPassword",
   "broker.hivemq.com",  // MQTT Broker server ip
   "MQTTUsername",       // Can be omitted if not needed
   "MQTTPassword",       // Can be omitted if not needed
@@ -26,7 +26,9 @@ void setup()
   // Optionnal functionnalities of EspMQTTClient : 
   client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
   //client.enableHTTPWebUpdater(); // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overrited with enableHTTPWebUpdater("user", "password").
-  client.enableLastWillMessage("TestClient/lastwill", "I am going offline");  // You can activate the retain flag by setting the third parameter to true
+  //client.enableLastWillMessage("TestClient/lastwill", "I am going offline");  // You can activate the retain flag by setting the third parameter to true
+  
+
 }
 
 // This function is called once everything is connected (Wifi and MQTT)
@@ -36,18 +38,30 @@ void onConnectionEstablished()
   
   // Subscribe to "mytopic/test" and display received message to Serial
   client.subscribe(topic, [](const String & payload) {
-    //Serial.println(payload);
+    if(payload != NULL)
+    {
+      msg["message"]=payload;
+      msg["time"] = 135248;
+
+      serializeJsonPretty(msg,Serial); 
+    }
   });
 }
 
 void loop()
 {
-  //client.loop();
   String messagetopub=Serial.readString();  
   client.loop();
-  if(messagetopub!= NULL ){
-         // Publish a message to "mytopic/test"
-    client.publish(topic, messagetopub); // You can activate the retain flag by setting the third parameter to true
+ 
+  if(messagetopub!= NULL )
+  {
+    // Publish a message to "mytopic/test"
+    client.publish(topic, messagetopub);
+    
+    msg["message"]=messagetopub;
+    msg["time"] = 135248;
+
+    serializeJsonPretty(msg,Serial); 
   }
   
 
